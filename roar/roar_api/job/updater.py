cce24bcbacd018e5c4407e96f5ead02def0f5a06
jimmy <jimmybob850513@gmail.com>
2024-08-12 00:03:25 +0800

    all

--- roar/roar_api/job/updater.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from .job import print_result
-from apscheduler.schedulers.background import BackgroundScheduler
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(print_result, 'cron', hour=17, minute=0, id='cron_task')
-    scheduler.start()
